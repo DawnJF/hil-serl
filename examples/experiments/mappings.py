18f11343801f4b08f32b24b58ad7248ca0fc3275
Joe618 <zoubh1997@yeah.net>
2025-10-28 10:19:44 +0800

    feat: make use of the various env configs by passing parameter

--- examples/experiments/mappings.py
+++ examples/experiments/mappings.py
@@ -2,10 +2,19 @@
 from experiments.usb_pickup_insertion.config import TrainConfig as USBPickupInsertionTrainConfig
 # from experiments.object_handover.config import TrainConfig as ObjectHandoverTrainConfig
 # from experiments.egg_flip.config import TrainConfig as EggFlipTrainConfig
+from experiments.usb_pickup_insertion.config_iphone import TrainConfig as IphoneChargingTrainConfig
+from experiments.usb_pickup_insertion.config_plug import TrainConfig as PlugintoSocketwithPowerCordTrainConfig
+from experiments.usb_pickup_insertion.config_fixed_pos import TrainConfig as PlugintoFixedSocketTrainConfig
+from experiments.usb_pickup_insertion.config_flexible import TrainConfig as PlugintoMovableSocketTrainConfig
+
 
 CONFIG_MAPPING = {
                 # "ram_insertion": RAMInsertionTrainConfig,
                 "usb_pickup_insertion": USBPickupInsertionTrainConfig,
                 # "object_handover": ObjectHandoverTrainConfig,
                 # "egg_flip": EggFlipTrainConfig,
-               }
+                "iphone_charging": IphoneChargingTrainConfig,
+                "plug_into_socket_with_power_cord": PlugintoSocketwithPowerCordTrainConfig,
+                "plug_into_fixed_socket": PlugintoFixedSocketTrainConfig,
+                "plug_into_movable_socket": PlugintoMovableSocketTrainConfig,
+                }
