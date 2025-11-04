3d749627214a2ce3035000b4f28da58aa834b57f
Jianfei Ma <majianfei1001@gmail.com>
2025-11-04 14:28:01 +0800

    add bash

---
+++ rl/run_learner.sh
@@ -0,0 +1,6 @@
+export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
+python rl/train.py "$@" \
+    --exp_name=plug_into_socket_with_power_cord \
+    --checkpoint_path=/home/facelesswei/code/hil-serl/outputs/rlpd_checkpoint/plug_into_socket_with_power_cord \
+    --learner \
